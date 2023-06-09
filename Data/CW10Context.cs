0ddd16682ba46897b94b020cfc5d2dfc34bd8d5c
Filip Michalski <118136393+pncqq@users.noreply.github.com>
2023-06-09 11:37:24 +0200

    Add files via upload

---
+++ Data/CW10Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CW10.Models;
+
+namespace CW10.Data;
+
+public class CW10Context : DbContext
+{
+    public CW10Context(DbContextOptions<CW10Context> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Movie> Movie { get; set; } = default!;
+
+   
+}
